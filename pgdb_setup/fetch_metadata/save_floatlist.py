f33f1493484253c33895e06adae5eeeb5424025d
Your Name <prshnnt@gmail.com>
2025-09-29 08:01:49 +0530

    created pgdb_setup folder to database setup

--- pgdb_setup/fetch_metadata/save_floatlist.py
+++
@@ -1,7 +0,0 @@
-from utils import ArgoMetadataExtractor
-
-extractor: ArgoMetadataExtractor = ArgoMetadataExtractor()
-float_list = extractor.get_float_list()
-
-extractor.save_float_list(float_list, "float_list.csv")
-len(float_list)
